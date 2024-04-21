59d1abfb6376f2f29f5c61b2424db19fc5c6a7ca
ShackuOttawa <spark188@uottawa.ca>
2024-04-21 04:49:44 -0400

    Probably good to go

--- projet24.pl
+++ projet24.pl
@@ -1,10 +1,16 @@
 % dataset(DirectoryName)
 % this is where the image dataset is located
-dataset("./imageDataset2_15_20").
+% locally defined for your convenience. no path switching required
+dataset("./imageDataset2_15_20/").
+
+% location of histogram q00 for quick access
+% also locally defined
+q00("./queryImagesWithHistograms/queryImages/q00.jpg.txt").
+
 % directory_textfiles(DirectoryName, ListOfTextfiles)
 % produces the list of text files in a directory
 directory_textfiles(D,Textfiles):- directory_files(D,Files), include(isTextFile, Files, Textfiles).
-isTextFile(Filename):-string_concat(_,'.txt',Filename).
+isTextFile(Filename):- string_concat(_,'.txt',Filename).
 % read_hist_file(Filename,ListOfNumbers)
 % reads a histogram file and produces a list of numbers (bin values)
 read_hist_file(Filename,Numbers):- open(Filename,read,Stream),read_line_to_string(Stream,_),
@@ -25,12 +31,37 @@ similarity_search(QueryFile,DatasetDirectory, DatasetFiles,Best):- read_hist_fil
 
 % compare_histograms(QueryHisto,DatasetDirectory,DatasetFiles,Scores)
 % compares a query histogram with a list of histogram files 
-compare_histograms(...?
+compare_histograms(QueryHisto, DatasetDirectory, DatasetFiles, Scores):-
+    
+    % Call supplementary comparer that will do most of the work
+    compare_histograms(QueryHisto, DatasetDirectory, DatasetFiles, [], Scores).
+
+% supplementary comparer. compares every file to the queryhisto, then returns the scores
+% in doing so, it calls the relevant sub-predicates
+compare_histograms(_, _, [], Scores, Scores).
+compare_histograms(QueryHisto, DatasetDirectory, [Headfile|Tailfiles], TempScores, Scores):-    
+
+    string_concat(DatasetDirectory, Headfile, Currentfile),
+    read_hist_file(Currentfile, Currenthistfile),
+    histogram_intersection(QueryHisto, Currenthistfile, Currentscore),
+    compare_histograms(QueryHisto, DatasetDirectory, Tailfiles, [(Headfile, Currentscore)|TempScores], Scores).
 
 % histogram_intersection(Histogram1, Histogram2, Score)
 % compute the intersection similarity score between two histograms
 % Score is between 0.0 and 1.0 (1.0 for identical histograms)
-histogram_intersection(H1,H2,S):- ... ?
+histogram_intersection(H1,H2,S):-
+    sum_list(H1, Pixels1), sum_list(H2, Pixels2),
+    calculate_score(H1, H2, Pixels1, Pixels2, 0, S).
+
+% histogram_intersection mainly just gathers the sum of the pixels for calculation
+% this predicate calculates the score of the query histogram 1 and another histogram 2.
+% end case return the Tempscore as final score S
+calculate_score([], [], _, _, S, S).
+% normalize heads, add the smallest number to the Tempscore
+calculate_score([Head1|Tail1], [Head2|Tail2], Pixels1, Pixels2, TempScore, S):-
+    NH1 is Head1 / Pixels1, NH2 is Head2 / Pixels2,
+    NewTemp is TempScore + min(NH1, NH2),
+    calculate_score(Tail1, Tail2, Pixels1, Pixels2, NewTemp, S).
 
 % take(List,K,KList)
 % extracts the K first items in a list
